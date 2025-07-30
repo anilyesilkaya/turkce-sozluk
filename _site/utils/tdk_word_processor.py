71581ee5d72135e4c7b2d7cc8f24dcdf730fe85e
Anil Yesilkaya <5503399+anilyesilkaya@users.noreply.github.com>
2025-07-30 09:14:12 +0100

    Finishing the v1.0. (i) Dictionary JSON creation and JSON2MD scripts have been developed.(ii) House keeping.

--- _site/utils/tdk_word_processor.py
+++ _site/utils/tdk_word_processor.py
@@ -3,30 +3,20 @@ import copy
 
 # Open load the TDK Sozluk JSON file
 data = []
-with open('database_sozluk/test2.json', 'r', encoding='utf-8') as file:
+with open('database_sozluk/v12/gts.json', 'r', encoding='utf-8') as file:
     for line in file:
         if line.strip(): # keep the none-empty lines
             data.append(json.loads(line))
-
-# #-------
-# # TEST
-# #--------
-# data = []
-# sozluk = {}
-# with open('database_sozluk/test.json', 'r', encoding='utf-8') as file:
-#     data = json.load(file)
-#     data = [data]
-# #-------
-# # TEST
-# #--------
 
 # Process the data
 sozluk = {}
 anlamlar = []
 ozellikler = []
 ornekler = []
+orneklerkaynak = []
 o_tmp = []
 e_tmp = []
+s_tmp = []
 print(f'(Progress...0%)') # initial progress display
 for i, entry in enumerate(data, start=1):
     # Reset the states
@@ -36,6 +26,7 @@ for i, entry in enumerate(data, start=1):
     anlamlar.clear()
     ozellikler.clear()
     ornekler.clear()
+    orneklerkaynak.clear()
     
     word = entry.get("madde","") # fetch the word
     if not word: # if the word is empty, skip
@@ -68,28 +59,39 @@ for i, entry in enumerate(data, start=1):
         ozellikler.append(o_tmp.copy())
 
         # Extract the examples for each anlam
-        if word != pword:
-            e_tmp.clear()
+        e_tmp.clear()
+        s_tmp.clear()
         if len(anlam.get("orneklerListe", "")) > 0:
             for e in anlam.get("orneklerListe", ""):
                 e_tmp.append(e.get("ornek", ""))
+                # If the yazar_id is different than 0 fetch the name of the author
+                if e.get("yazar",""):
+                    s_tmp.append(e.get("yazar", "")[0].get("tam_adi", ""))
+                else:
+                    s_tmp.append("")
         else:
             e_tmp.append("")
+            s_tmp.append("")
         ornekler.append(e_tmp.copy())
+        orneklerkaynak.append(s_tmp.copy())
 
-        # Create a dictionary using populated values, key is the word itself
-        sozluk[word] = {
-            "lisan": language,
-            "anlamlar": copy.deepcopy(anlamlar),
-            "ozellikler": copy.deepcopy(ozellikler),
-            "ornekler": copy.deepcopy(ornekler)
-        }
+        
+        # Update the word pointer
         pword = word
+        
+    # Create a dictionary using populated values, key is the word itself
+    sozluk[word] = {
+        "lisan": language,
+        "anlamlar": copy.deepcopy(anlamlar),
+        "ozellikler": copy.deepcopy(ozellikler),
+        "ornekler": copy.deepcopy(ornekler),
+        "orneklerkaynak": copy.deepcopy(orneklerkaynak)
+    }
 
     
     # Show progress
     print(f'(Progress...{100*i/len(data)}%)')
 
 # Save processed sozluk
-with open("database_sozluk/processed_sozluk.json", "w", encoding="utf-8") as f:
+with open("database_sozluk/v12/processed_gts.json", "w", encoding="utf-8") as f:
     json.dump(sozluk, f, ensure_ascii=False, indent=2)
